bf9c2e0233c153198be6afdf2beee06399164b9f
Łukasz Łućko <lukaszl95@o2.pl>
2018-03-16 14:09:27 +0100

    Zadanie-19.4

--- reducer.js
+++ reducer.js
@@ -1,6 +1,11 @@
 import { combineReducers } from "redux";
 import comments from "./comments";
-import users from "./users"
+import users from "./users";
+import { ADD_COMMENT } from "./action";
+import { EDIT_COMMENT } from "./action";
+import { REMOVE_COMMENT } from "./action";
+import { THUMB_UP_COMMENT_COMMENT } from "./action";
+import { THUMB_DOWN_COMMENT_COMMENT } from "./action";
 
 const app = combineReducers({
 	comments,
